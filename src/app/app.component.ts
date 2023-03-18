2795b54d796c6bfd157c9f31e9bb1d8733215183
Gadhiya Akshaykumar (DC-MH/SDI) <gaa1ul@bosch.com>
2023-03-18 22:40:41 +0100

    Setup project structure and design home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,81 +8,97 @@ import { environment } from 'src/assets/environment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mynewApp';
-  httpHeader: any;
-  auth_token: any = environment.auth_token;
-  GetPersonDetails: any;
-  addperson: any= FormGroup;
-  rowsPerPage: number = 10;
-  actualPage: number = 1;
-  showprevnext: boolean = false;
-  endPaging: any;
-  showPersonDetails: any;
+  // title = 'mynewApp';
+  // httpHeader: any;
+  // auth_token: any = environment.auth_token;
+  // GetPersonDetails: any;
+  // addperson: any= FormGroup;
+  // rowsPerPage: number = 10;
+  // actualPage: number = 1;
+  // showprevnext: boolean = false;
+  // endPaging: any;
+  // showPersonDetails: any;
+  // months: string[] = ["january", "february", "march", "april", "may", "june", "july", "August", "sepetember", "octomber", "november", "december"]
   
-
+  // currentPos : number=2;
+  // currentContent: string =this.months[this.currentPos];
   constructor(private http: HttpClient, private formBuilder: FormBuilder){
-    this.httpHeader = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${this.auth_token}`
-    });
+    // this.httpHeader = new HttpHeaders({
+    //   'Content-Type': 'application/json',
+    //   'Authorization': `Bearer ${this.auth_token}`
+    // });
   }
 
   ngOnInit(): void {
-    this.http
-    .get(environment.api, {headers: this.httpHeader})
-    .subscribe((res: any) => {
-      this.GetPersonDetails = res;
-      this.pagination();
-        this.updateViewAfterPaginationClicked();
-        // console.log(res);
-    });
+    // this.http
+    // .get(environment.api, {headers: this.httpHeader})
+    // .subscribe((res: any) => {
+    //   this.GetPersonDetails = res;
+    //   this.pagination();
+    //     this.updateViewAfterPaginationClicked();
+    //     // console.log(res);
+    // });
 
-    this.addperson = this.formBuilder.group({
-      name: ['', Validators.required],
-      username: ['', Validators.required],
-      email: ['', Validators.required]
-    });
-    this.pagination();
+    // this.addperson = this.formBuilder.group({
+    //   name: ['', Validators.required],
+    //   username: ['', Validators.required],
+    //   email: ['', Validators.required]
+    // });
+    // this.pagination();
   }
+  // toggleContent(val: any){
+  //   if(val < 0 && this.currentPos == 0){
+  //     this.currentPos = 11;
+  //     this.currentContent = this.months[this.currentPos]
+  //   }else if(val > 0 && this.currentPos == 11){
+  //     this.currentPos = 0;
+  //     this.currentContent = this.months[this.currentPos]
+  //   }else{
+  //     this.currentPos=this.currentPos+val;
+  //     this.currentContent = this.months[this.currentPos];
+  //   }
+  // }
 
-  createNewPerson(){
-    // console.log(this.addperson.value);
+  
 
-    this.http
-    .post(environment.api, this.addperson.value, {headers: this.httpHeader})
-    .subscribe((res: any) => {
-      this.GetPersonDetails = res;
-        console.log(res);
-        this.ngOnInit();
-    });
+  // createNewPerson(){
+  //   // console.log(this.addperson.value);
 
-  }
+  //   this.http
+  //   .post(environment.api, this.addperson.value, {headers: this.httpHeader})
+  //   .subscribe((res: any) => {
+  //     this.GetPersonDetails = res;
+  //       console.log(res);
+  //       this.ngOnInit();
+  //   });
 
-  pagination() {
-    if (this.GetPersonDetails?.length % this.rowsPerPage != 0) {
-      this.endPaging = Math.round(this.GetPersonDetails?.length / this.rowsPerPage)
-    } else {
-      this.endPaging = this.GetPersonDetails.length / this.rowsPerPage
-    }
-  }
-  onPageChange(value: any) {
-    this.showprevnext = false;
-    this.actualPage = value;
-    this.updateViewAfterPaginationClicked();
-  }
-  onChangeRowsPerPage(value: any) {
-    this.rowsPerPage = value;
-    this.pagination();
-    this.actualPage = 1;
-    this.updateViewAfterPaginationClicked();
-  }
-  updateViewAfterPaginationClicked() {
-    var start = (this.actualPage - 1) * this.rowsPerPage;
-    var end = this.actualPage * this.rowsPerPage - 1;
-    this.showPersonDetails = this.GetPersonDetails?.slice(start, end);
-  }
+  // }
 
-  prevnext(){
-    this.showprevnext = true;
-  }
+  // pagination() {
+  //   if (this.GetPersonDetails?.length % this.rowsPerPage != 0) {
+  //     this.endPaging = Math.round(this.GetPersonDetails?.length / this.rowsPerPage)
+  //   } else {
+  //     this.endPaging = this.GetPersonDetails.length / this.rowsPerPage
+  //   }
+  // }
+  // onPageChange(value: any) {
+  //   this.showprevnext = false;
+  //   this.actualPage = value;
+  //   this.updateViewAfterPaginationClicked();
+  // }
+  // onChangeRowsPerPage(value: any) {
+  //   this.rowsPerPage = value;
+  //   this.pagination();
+  //   this.actualPage = 1;
+  //   this.updateViewAfterPaginationClicked();
+  // }
+  // updateViewAfterPaginationClicked() {
+  //   var start = (this.actualPage - 1) * this.rowsPerPage;
+  //   var end = this.actualPage * this.rowsPerPage - 1;
+  //   this.showPersonDetails = this.GetPersonDetails?.slice(start, end);
+  // }
+
+  // prevnext(){
+  //   this.showprevnext = true;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,38 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+
+//  Angular Material Components
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
